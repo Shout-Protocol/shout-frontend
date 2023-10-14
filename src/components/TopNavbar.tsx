9ce9188090ce90271164a72f8f70c4d839606b56
Warun S <warunsinx@gmail.com>
2023-10-14 21:54:25 +0700

    style: :lipstick: up-scale shout logo text

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -20,7 +20,9 @@ export default function TopNavbar() {
           className="select-none cursor-pointer flex items-center"
         >
           <Image src="/shout.svg" alt="Shout Logo" width="30" height="30" />
-          <p className="text-gray-800 font-bold text-xl ml-3">shout protocol</p>
+          <p className="text-gray-800 font-bold text-2xl ml-3">
+            shout protocol
+          </p>
         </div>
       </NavbarBrand>
       <NavbarContent justify="end">
