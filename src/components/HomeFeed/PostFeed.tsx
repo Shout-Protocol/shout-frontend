a5ad41f9e8077e779bc9acae35efdfa9d0efc12a
Warun S <warunsinx@gmail.com>
2023-10-22 03:51:52 +0700

    refactor: :zap: move create post to index component

--- src/components/HomeFeed/PostFeed.tsx
+++ src/components/HomeFeed/PostFeed.tsx
@@ -9,7 +9,6 @@ export default function PostFeed() {
 
   return (
     <div>
-      <CreatePost />
       <FeedSelector
         selected={feedType}
         setSelected={setFeedType}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import CreatePost from "@/components/CreatePost/CreatePost";
 import PostFeed from "@/components/HomeFeed/PostFeed";
 
 export default function Home() {
   return (
     <div>
+      <CreatePost />
       <PostFeed />
     </div>
   );
