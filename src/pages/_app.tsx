785486649c0fd39c68beb960a9c4b9f10474ce24
Warun S <warunsinx@gmail.com>
2023-10-22 03:47:51 +0700

    feat: :zap: setup apollo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
 import MainLayout from "@/components/Layout/MainLayout";
+import ApolloClientProvider from "@/providers/ApolloClientProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
-    </NextUIProvider>
+    <ApolloClientProvider>
+      <NextUIProvider>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </NextUIProvider>
+    </ApolloClientProvider>
   );
 }
