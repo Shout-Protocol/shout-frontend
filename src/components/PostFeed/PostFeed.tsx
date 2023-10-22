a2b47c6dc0c151c6b381191480ac9d0bc4363420
Warun S <warunsinx@gmail.com>
2023-10-22 16:47:37 +0700

    feat: :zap: update post feed

--- src/components/PostFeed/PostFeed.tsx
+++ src/components/PostFeed/PostFeed.tsx
@@ -2,10 +2,22 @@ import React, { useState } from "react";
 import PostItem from "./PostItem";
 import FeedSelector from "./FeedSelector";
 import { FEED_TYPE_MENU } from "@/constants/menu";
+import { useQuery } from "@apollo/client";
+import { GET_POSTS } from "@/gql/post.query";
+import { Post } from "../../../types/shoutGQL";
+import { Spinner } from "@nextui-org/react";
 
 export default function PostFeed() {
-  const [feedType, setFeedType] =
-    useState<keyof typeof FEED_TYPE_MENU>("Spark");
+  const [feedType, setFeedType] = useState<keyof typeof FEED_TYPE_MENU>("All");
+  const { loading, error, data } = useQuery<{ posts: Post[] }>(GET_POSTS);
+  console.log(data);
+
+  if (loading)
+    return (
+      <div className="flex w-full items-center justify-center h-[200px]">
+        <Spinner />
+      </div>
+    );
 
   return (
     <div>
